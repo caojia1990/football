b5368e8153b51af2020069fd2ee7f62c306a2c22
caoji_000 <caoji_000@caojia>
2016-08-02 23:05:56 +0800

    启动脚本

---
+++ eastng-football-service/bin/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd `dirname $0`
+cd ..
+DEPLOY_DIR=`pwd`
+LIB_DIR=DEPLOY_DIR/lib
+LIB_JARS=`ls $LIB_DIR|grep .jar|awk '{print "'$LIB_DIR'/"$0}'|tr "\n" ":"`
+
+
+nohup java  -classpath $CONF_DIR:$LIB_JARS com.eastng.football.main.FootballApplication > 
+
+
