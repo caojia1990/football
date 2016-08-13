1021601cfd61ba8cb56c18e2e89d1879128f784f
caoji_000 <caoji_000@192.168.65.1>
2016-08-13 23:11:47 +0800

    更新mapper.xml

--- eastng-football-service/bin/start.sh
+++ eastng-football-service/bin/start.sh
@@ -2,10 +2,10 @@
 cd `dirname $0`
 cd ..
 DEPLOY_DIR=`pwd`
-LIB_DIR=DEPLOY_DIR/lib
+LIB_DIR=$DEPLOY_DIR/lib
 LIB_JARS=`ls $LIB_DIR|grep .jar|awk '{print "'$LIB_DIR'/"$0}'|tr "\n" ":"`
 
 
-nohup java  -classpath $CONF_DIR:$LIB_JARS com.eastng.football.main.FootballApplication > stdout.log
+nohup java  -classpath $LIB_JARS com.eastng.football.main.FootballApplication > stdout.log
 
 
