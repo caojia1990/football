1169198c7734196f89e836732bf525f80e967bf6
caoji_000 <caoji_000@caojia>
2016-09-10 10:13:42 +0800

    查询积分榜  2016-09-10

---
+++ eastng-football-web/src/main/java/com/eastng/football/web/view/dubbo/WDubboParameter.java
@@ -0,0 +1,19 @@
+package com.eastng.football.web.view.dubbo;
+
+public class WDubboParameter {
+
+    /**
+     * 注册中心地址
+     */
+    private String registryAddress;
+    
+    /**
+     * 接口名
+     */
+    private String referenceInterface;
+    
+    /**
+     * 版本号
+     */
+    private String version;
+}
